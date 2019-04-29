37f1d98ad353908064b37bbccdb4a1b2d14a13ae
rotimi-babalola <rotimi.babalola@andela.com>
2019-04-29 23:44:55 +0100

    add header image and component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './common/Header';
 
 import '../styles/scss/app.scss';
 
-const App = () => <div className="wrapper">kfkfk</div>;
+const App = () => (
+  <div className="wrapper">
+    <Header />
+  </div>
+);
 
 export default App;
---
+++ client/src/components/common/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <a href="/">
+      <img
+        src="client/src/images/logo.png"
+        alt="Pie in the Sky Bakery, Springdale, VT. Click for home."
+      />
+    </a>
+  </header>
+);
+
+export default Header;
